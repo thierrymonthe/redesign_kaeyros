70a32c1036ad0843d5606d6d8d87eabe287fc80a
ndjaka <eugene.ndjaka@kaeyros-analytics.de>
2022-03-08 16:50:08 +0100

    content

--- src/app/landing-page1/components/section-vertical-two/section-vertical-two.component.ts
+++ src/app/landing-page1/components/section-vertical-two/section-vertical-two.component.ts
@@ -9,7 +9,7 @@ export class SectionVerticalTwoComponent implements OnInit {
   titleSectionProp: any = {
     class: 'text-left',
     title: 'How We Work',
-    subTitle: 'See Xamin In Action',
+    subTitle: 'See Our Statistics',
     // tslint:disable-next-line:max-line-length
     description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.It is a long established fact.'
   };
--- src/app/xamin/components/team/team.component.ts
+++ src/app/xamin/components/team/team.component.ts
@@ -11,48 +11,72 @@ export class TeamComponent implements OnInit {
       img:  "a1.jpg",
       name: "Thierry MONTHE",
       position: "CEO - Chief Data Scientist",
+      twitter_link: "#ff",
+      fb_link: "#",
+      linkedin: "#",
 
     },
     {
       img:  "a1.jpg",
       name: "Patrice KWEMO",
       position: "CTO",
+      twitter_link: "#ff2",
+      fb_link: "#",
+      linkedin: "#",
 
     },
     {
       img:  "a1.jpg",
       name: "Gleyne MONTHE",
       position: "Data Scientist",
+      twitter_link: "#ff2",
+      fb_link: "#",
+      linkedin: "#",
 
     },
     {
       img:  "a1.jpg",
       name: "Landry TANDJEU",
       position: "Project Manager",
+      twitter_link: "#ff2",
+      fb_link: "#",
+      linkedin: "#",
 
     },
     {
       img:  "a2.jpg",
       name: "Agnes AKANKWASA",
-      position: "Data Scientist",
+      position: "Working Student - Data Science",
+      twitter_link: "#ff2",
+      fb_link: "#",
+      linkedin: "https://www.linkedin.com/in/agnes-akankwasa-29259410a/",
 
     },
     {
       img:  "a1.jpg",
       name: "Yvan KOMBOU",
       position: "Web Developer",
+      twitter_link: "#ff2",
+      fb_link: "#",
+      linkedin: "#",
 
     },
     {
-      img:  "a1.jpg",
-      name: "Eugene Ndjaka",
+      img:  "eugene.jpg",
+      name: "Eugène NDJAKA",
       position: "Web Developer",
+      twitter_link: "#ff2",
+      fb_link: "#",
+      linkedin: "www.linkedin.com/in/eugène-ndjaka-a83b20154",
 
     },
     {
       img:  "me.jpg",
       name: "Pierre TOUBE",
       position: "Web Developer",
+      twitter_link: "#ff2",
+      fb_link: "#",
+      linkedin: "#",
 
     }
     ];
